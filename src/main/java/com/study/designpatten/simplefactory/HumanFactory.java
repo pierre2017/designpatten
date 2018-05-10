19dd36331adc70e41b68863f286d84217109d5e1
pierre2017 <dsc419@163.com>
2018-05-10 13:57:15 +0800

    简单工厂方法

--- src/main/java/com/study/designpatten/simplefactory/HumanFactory.java
+++ src/main/java/com/study/designpatten/simplefactory/HumanFactory.java
@@ -2,10 +2,12 @@ package com.study.designpatten.simplefactory;
 
 import com.study.designpatten.factorymethod.Human;
 
+import java.io.Serializable;
+
 /**
  * Created by pierre on 2018/5/9.
  */
-public class HumanFactory {
+public class HumanFactory implements Serializable {
     public static <T extends Human> T createHuman(Class<T> c) {
         Human human = null;
         try {
